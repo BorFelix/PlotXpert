00e888e31b68f6215550c7d8b0749ab125709498
Felix <446311061@qq.com>
2023-10-08 13:06:35 +0800

    Add Description of file

--- PlotXpert/coreEngine/src/CoreEngine/CoreEngine.cxx
+++ PlotXpert/coreEngine/src/CoreEngine/CoreEngine.cxx
@@ -1,22 +1,28 @@
+// Felix Bao, Initialization
 #include <CoreEngine.hxx>
 
-void CoreEngine::execute(IUnaryOperation& operation, IPointSeries& input, OperationResult& result) {
-	operation.execute(input, result);  // Mock execution for unary operation
+void CoreEngine::execute(IUnaryOperation &operation, IPointSeries &input, OperationResult &result)
+{
+	operation.execute(input, result); // Mock execution for unary operation
 }
 
-void CoreEngine::execute(IBinaryOperation& operation, IPointSeries& input1, IPointSeries& input2, OperationResult& result) {
-	operation.execute(input1, input2, result);  // Mock execution for binary operation
+void CoreEngine::execute(IBinaryOperation &operation, IPointSeries &input1, IPointSeries &input2, OperationResult &result)
+{
+	operation.execute(input1, input2, result); // Mock execution for binary operation
 }
 
-void CoreEngine::execute(IUnaryOperation& operation, const std::vector<IPointSeries*>& input, OperationResult& result) {
-	operation.execute(input, result);  // Mock execution for unary operation with vector
+void CoreEngine::execute(IUnaryOperation &operation, const std::vector<IPointSeries *> &input, OperationResult &result)
+{
+	operation.execute(input, result); // Mock execution for unary operation with vector
 }
 
-void CoreEngine::execute(IBinaryOperation& operation, const std::vector<IPointSeries*>& input1, const std::vector<IPointSeries*>& input2, OperationResult& result) {
-	operation.execute(input1, input2, result);  // Mock execution for binary operation with vectors
+void CoreEngine::execute(IBinaryOperation &operation, const std::vector<IPointSeries *> &input1, const std::vector<IPointSeries *> &input2, OperationResult &result)
+{
+	operation.execute(input1, input2, result); // Mock execution for binary operation with vectors
 }
 
-template<typename... T>
-void CoreEngine::execute(ICustomExtendOperation<T...>& operation, const std::vector<CustomPointSeries<T...>*>& data, OperationResult& result) {
-	operation.execute(data, result);  // Mock execution for custom operations
+template <typename... T>
+void CoreEngine::execute(ICustomExtendOperation<T...> &operation, const std::vector<CustomPointSeries<T...> *> &data, OperationResult &result)
+{
+	operation.execute(data, result); // Mock execution for custom operations
 }
